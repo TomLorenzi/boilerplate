ffde81968ad8435b20566e4552491aedb9bf01f4
Thomas De Lorenzi <thomas.delorenzi@gmail.com>
2024-02-19 00:09:51 +0100

    fix react router and so tailwind

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -17,7 +17,7 @@ export default function Auth() {
       <div className="md:hidden">
         
       </div>
-      <div className="container relative hidden h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <div className="container relative hidden h-[100vh] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
         <Link
           href="/examples/authentication"
           className={cn(
